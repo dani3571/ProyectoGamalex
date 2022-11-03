3981be85b2649c5b47c7e928f41dffaad4d6ca8f
Adrian Herrera <adrianhlinares@gmail.com>
2022-11-03 05:29:55 -0400

    Crud ventas

--- CRUDs/Ventas/insertarVenta.php
+++ CRUDs/Ventas/insertarVenta.php
@@ -2,15 +2,22 @@
 include("../conexion.php");
 $con=conectar();
 
-$IdLaboratorio=$_POST['IdLaboratorio'];
-$Nombre=$_POST['Nombre'];
-$Direccion=$_POST['Direccion'];
-
-$sql="INSERT INTO laboratorio VALUES('$IdLaboratorio','$Nombre','$Direccion')";
+$Producto=$_POST['Producto'];
+$sql = "SELECT *  FROM producto where Nombre = '$Producto'";
 $query= mysqli_query($con,$sql);
+$row=mysqli_fetch_array($query);
+$IdProducto = $row['IdProducto'];
+$IdUsuario = 1;
+$IdCliente = 1;
+$Estado = 1;
+$FechaVenta = '2022-11-03 04:09:01';
+$Cantidad=$_POST['Cantidad'];
 
-if($query){
-    Header("Location: alumno.php");
+$sql2="INSERT INTO venta (IdProducto, IdUsuario, IdCliente, Estado, FechaVenta, Cantidad) VALUES ($IdProducto,$IdUsuario,$IdCliente,$Estado,'$FechaVenta',$Cantidad)";
+$query2= mysqli_query($con,$sql2);
+
+if($query2){
+    Header("Location: Index.php");
 }else {
     
 }
