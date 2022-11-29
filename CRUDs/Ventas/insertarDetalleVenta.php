b6f7480816e9f6eda9c41dce5434047181abda7d
Daniel Chavez <CCD2024081@est.univalle.edu>
2022-11-29 16:30:33 -0400

    Ventas

--- CRUDs/Ventas/insertarDetalleVenta.php
+++ CRUDs/Ventas/insertarDetalleVenta.php
@@ -9,9 +9,5 @@
     $query= mysqli_query($con,$sql);
     $sql2 ="UPDATE producto SET Cantidad = Cantidad - '$Cantidad' WHERE IdProducto='$IdProducto'";
     $query2 = mysqli_query($con,$sql2);
-    if($query){
-        Header("Location: Index.php");
-    }else {
-        
-    }
+    
 ?>
