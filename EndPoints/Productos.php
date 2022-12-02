34fc3b59e3d1d6e51fa6bfd9aff09e52c2b5b101
acreedx <adrianhlinares@gmail.com>
2022-12-02 16:12:32 -0400

    acreedx

--- EndPoints/Productos.php
+++ EndPoints/Productos.php
@@ -2,14 +2,12 @@
  
     include("./conexion.php");
     $con=conectar();
-    $sql="SELECT * FROM producto";
+    $sql="SELECT IdProducto,Nombre,Cantidad,PrecioUnidad,PrecioTotalProducto,Descripcion,Estado,IdLaboratorio FROM producto";
     $query=mysqli_query($con,$sql);
-    $total_rows = $query -> num_rows;
-    if($total_rows > 0){
-        while($productos = mysqli_fetch_array($query)){
-            $data[] = $productos;
-            
-        }
-        echo json_encode($data);
+    $data = array();
+    while($row = mysqli_fetch_assoc($query))
+    {
+        $data[] = $row;
     }
+    print json_encode($data);
 ?>
