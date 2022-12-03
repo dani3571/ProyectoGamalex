7868432fd985ef297fbbfe70ba6c497419b1068b
Daniel <ccs2024081@est.univalle.edu>
2022-12-03 09:37:05 -0400

    Arreglo de main container css

--- CRUDs/Clasificar/Index.php
+++ CRUDs/Clasificar/Index.php
@@ -7,7 +7,7 @@ include("/xampp/htdocs/ProyectoGamalex/EstructuraCuerpo/P.php");
         <title>Categorias</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../css/b.css">
+        <link rel="stylesheet" href="../css/newStyles.css">
         <script src="https://cdn.tailwindcss.com"></script>
        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
